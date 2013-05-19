43e72d0806640e1dc0d15f4fc764deb8c235e7d4
Dan Mulloy <dmulloy2@live.com>
2013-05-18 22:14:57 -0400

    Fix activation bug

--- src/main/java/net/dmulloy2/swornrpg/AbilitiesManager.java
+++ src/main/java/net/dmulloy2/swornrpg/AbilitiesManager.java
@@ -56,7 +56,7 @@ public class AbilitiesManager
 		{
 			for (Action action : actions)
 			{
-				if (action == Action.LEFT_CLICK_AIR || action == Action.PHYSICAL)
+				if (action == Action.LEFT_CLICK_AIR || action == Action.LEFT_CLICK_BLOCK || action == Action.PHYSICAL)
 					activate = true;
 			}
 		}
