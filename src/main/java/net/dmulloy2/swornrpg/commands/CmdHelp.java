9efb08d917abc78443fe9c55c2b29d69a2088415
Dan Mulloy <dmulloy2@live.com>
2013-08-09 23:28:47 -0400

    Pagination.

--- src/main/java/net/dmulloy2/swornrpg/commands/CmdHelp.java
+++ src/main/java/net/dmulloy2/swornrpg/commands/CmdHelp.java
@@ -1,102 +1,79 @@
 package net.dmulloy2.swornrpg.commands;
 
 import java.util.ArrayList;
-import net.dmulloy2.swornrpg.SwornRPG;
+import java.util.List;
 
-public class CmdHelp extends SwornRPGCommand
+import net.dmulloy2.swornrpg.SwornRPG;
+import net.dmulloy2.swornrpg.util.FormatUtil;
+
+/**
+ * @author dmulloy2
+ */
+
+public class CmdHelp extends PaginatedCommand 
 {
 	public CmdHelp(SwornRPG plugin)
 	{
 		super(plugin);
 		this.name = "help";
-		this.aliases.add("h");
-		this.description = "Display SwornRPG help";
 		this.optionalArgs.add("page");
+		this.description = "Shows " + plugin.getName() + " help";
+		this.linesPerPage = 6;
+		
 		this.usesPrefix = true;
-	}	
+	}
+
+	@Override
+	public int getListSize() 
+	{
+		return buildHelpMenu().size();
+	}
+
+	@Override
+	public String getHeader(int index) 
+	{
+		return FormatUtil.format(plugin.getMessage("help_header"), index, getPageCount());
+	}
+
+	@Override
+	public List<String> getLines(int startIndex, int endIndex) 
+	{
+		List<String> lines = new ArrayList<String>();
+		for (int i = startIndex; i < endIndex && i < getListSize(); i++) 
+		{
+			lines.add(buildHelpMenu().get(i));
+		}
+		
+		return lines;
+	}
+
 	
 	@Override
-	public void perform()
+	public String getLine(int index) 
 	{
-		if (helpPages == null) 
-			updateHelp();
-		
-		int page = 0;
-		if (args.length > 0)
-		{
-			page = argAsInt(0, true);
-			
-			if (page == -1)
-				return;
-		}
-		
-		if (page == 0)
-			page = 1;
-		
-		sendMessage(plugin.getMessage("help_header"), page, helpPages.size());
-		
-		page -= 1;
-		
-		if (page < 0 || page >= helpPages.size())
-		{
-			err(plugin.getMessage("invalid_help_page"), args[0]);
-			return;
-		}
-		for (String string : helpPages.get(page))
-			sendMessage(string);
+		return null;
 	}
 	
-	/**Build the help pages**/
-	public ArrayList<ArrayList<String>> helpPages;
-	
-	public void updateHelp()
+	private final List<String> buildHelpMenu()
 	{
-		helpPages = new ArrayList<ArrayList<String>>();
-		ArrayList<String> pageLines;
-
-		pageLines = new ArrayList<String>();
-		pageLines.add(new CmdHelp(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdReload(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdVersion(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdLeaderboard(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdAChat(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdAddxp(plugin).getUsageTemplate(true));
-		helpPages.add(pageLines);
+		List<String> ret = new ArrayList<String>();
 		
-		pageLines = new ArrayList<String>();
-		pageLines.add(new CmdASay(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdCoordsToggle(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdDeny(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdDivorce(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdEject(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdFrenzy(plugin).getUsageTemplate(true));
-		helpPages.add(pageLines);
+		for (SwornRPGCommand cmd : plugin.getCommandHandler().getRegisteredPrefixedCommands())
+		{
+			if (plugin.getPermissionHandler().hasPermission(sender, cmd.permission))
+			{
+				ret.add(cmd.getUsageTemplate(true));
+			}
+		}
 		
-		pageLines = new ArrayList<String>();
-		pageLines.add(new CmdHighCouncil(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdItemName(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdLevel(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdLevelr(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdMarry(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdMatch(plugin).getUsageTemplate(true));
-		helpPages.add(pageLines);
-	
-		pageLines = new ArrayList<String>();
-		pageLines.add(new CmdMine(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdPropose(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdRide(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdSitdown(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdSpouse(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdStaffList(plugin).getUsageTemplate(true));	
-		helpPages.add(pageLines);
+		for (SwornRPGCommand cmd : plugin.getCommandHandler().getRegisteredCommands())
+		{
+			if (plugin.getPermissionHandler().hasPermission(sender, cmd.permission))
+			{
+				ret.add(cmd.getUsageTemplate(true));
+			}
+		}
 		
-		pageLines = new ArrayList<String>();
-		pageLines.add(new CmdStandup(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdTag(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdTagr(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdUnride(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdUnlimitedAmmo(plugin).getUsageTemplate(true));
-		pageLines.add(new CmdAbilities(plugin).getUsageTemplate(true));
-		helpPages.add(pageLines);
+		return ret;
 	}
 }
--- src/main/java/net/dmulloy2/swornrpg/commands/CmdLeaderboard.java
+++ src/main/java/net/dmulloy2/swornrpg/commands/CmdLeaderboard.java
@@ -8,11 +8,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.bukkit.OfflinePlayer;
 import net.dmulloy2.swornrpg.SwornRPG;
 import net.dmulloy2.swornrpg.data.PlayerData;
 import net.dmulloy2.swornrpg.util.FormatUtil;
 import net.dmulloy2.swornrpg.util.Util;
+
+import org.bukkit.OfflinePlayer;
 
 /**
  * @author dmulloy2
@@ -23,9 +24,9 @@ public class CmdLeaderboard extends SwornRPGCommand
 	public CmdLeaderboard (SwornRPG plugin)
 	{
 		super(plugin);
-		this.name = "top";
+		this.name = "lb";
 		this.description = "Display level leaderboard";
-		this.aliases.add("lb");
+		this.aliases.add("top");
 		this.usesPrefix = true;
 	}
 	
@@ -89,3 +90,142 @@ public class CmdLeaderboard extends SwornRPGCommand
 		sendMessage(plugin.getMessage("leaderboard_check"));
 	}
 }
+
+/*public class CmdLeaderboard extends PaginatedCommand
+{
+	private long lastUpdate;
+	private boolean hasBeenUpdated;
+	private List<String> leaderboard;
+	public CmdLeaderboard(SwornRPG plugin)
+	{
+		super(plugin);
+		this.name = "lb";
+		this.aliases.add("top");
+		this.optionalArgs.add("page");
+		this.description = "Displays level leaderboard";
+		this.linesPerPage = 10;
+		this.usesPrefix = true;
+		
+		this.lastUpdate = System.currentTimeMillis();
+		this.leaderboard = new ArrayList<String>();
+	}
+
+	@Override
+	public int getListSize() 
+	{
+		return leaderboard.size();
+	}
+
+	@Override
+	public String getHeader(int index) 
+	{
+		return FormatUtil.format(plugin.getMessage("leaderboard_header"), index, getPageCount());
+	}
+	
+	@Override
+	public void perform() 
+	{
+		if ((System.currentTimeMillis() - lastUpdate) >= 18000L || ! hasBeenUpdated)
+		{
+			sendpMessage(getMessage("leaderboard_wait"));
+			buildLeaderboard();
+		}
+		
+		int index = 1;
+		if (this.args.length > pageArgIndex) 
+		{
+			try 
+			{
+				index = Integer.parseInt(args[pageArgIndex]);
+				if (index < 1 || index > getPageCount())
+					throw new IndexOutOfBoundsException();
+			}
+			catch (NumberFormatException ex)
+			{
+				err(plugin.getMessage("error-invalid-page"), args[0]);
+				return;
+			}
+			catch (IndexOutOfBoundsException ex) 
+			{
+				err(plugin.getMessage("error-no-page-with-index"), args[0]);
+				return;
+			}
+		}
+		
+		List<String> page = getPage(index);
+		page.add(FormatUtil.format(plugin.getMessage("leaderboard_check")));
+		
+		for (String s : page)
+			sendMessage(s);
+	}
+	
+	public void buildLeaderboard()
+	{
+		leaderboard.clear();
+		
+		Map<String, PlayerData> data = plugin.getPlayerDataCache().getAllPlayerData();
+		HashMap<String, Integer> xpmap = new HashMap<String, Integer>();
+		for (Entry<String, PlayerData> entrySet : data.entrySet())
+		{
+			String player = entrySet.getKey();
+			PlayerData data1 = entrySet.getValue();
+			int xp = data1.getTotalxp();
+			if (xp > 0)
+				xpmap.put(player, xp);
+		}
+		
+		final List<Map.Entry<String, Integer>> sortedEntries = new ArrayList<Map.Entry<String, Integer>>(xpmap.entrySet());
+		Collections.sort(
+				sortedEntries, new Comparator<Map.Entry<String, Integer>>()
+				{
+					@Override
+					public int compare(final Entry<String, Integer> entry1, final Entry<String, Integer> entry2)
+					{
+						return -entry1.getValue().compareTo(entry2.getValue());
+					}
+				});
+
+		int pos = 1;
+		for (Map.Entry<String, Integer> entry : sortedEntries)
+		{
+			String string = entry.getKey();
+			OfflinePlayer player = Util.matchOfflinePlayer(string);
+			if (player != null)
+			{
+				PlayerData data2 = getPlayerData(player);
+				if (data2 != null)
+				{
+					int level = data2.getLevel();
+					int xp = data2.getTotalxp();
+							
+					StringBuilder line = new StringBuilder();
+					line.append(FormatUtil.format(plugin.getMessage("leaderboard_format"), pos, player.getName(), level, xp));
+					leaderboard.add(line.toString());
+					pos++;
+				}
+			}
+		}
+		
+		this.hasBeenUpdated = true;
+		this.lastUpdate = System.currentTimeMillis();
+	}
+	
+	@Override
+	public List<String> getLines(int startIndex, int endIndex) 
+	{
+		List<String> lines = new ArrayList<String>();
+		for (int i = startIndex; i < endIndex && i < getListSize(); i++) 
+		{
+			lines.add(leaderboard.get(i));
+		}
+		
+		return lines;
+	}
+
+	@Override
+	public String getLine(int index)
+	{
+		// Not needed since we override perform
+		return null;
+	}
+}*/
