02b62e706c34c8ef9cd483e88866482f1dca796b
Dan Mulloy <dmulloy2@live.com>
2013-11-17 22:57:41 -0500

    Forgot the Reloadable class...

---
+++ src/main/java/net/dmulloy2/swornrpg/types/Reloadable.java
@@ -0,0 +1,13 @@
+package net.dmulloy2.swornrpg.types;
+
+/**
+ * Classes implementing this Interface will be reloaded when the reload command
+ * is issued.
+ * 
+ * @author dmulloy2
+ */
+
+public interface Reloadable
+{
+	public abstract void reload();
+}
